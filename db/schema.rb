b08f9da9443ec9400379cd2276ec149e99b7d576
Melanie Yeoh <melanieyeoh@Melanies-MacBook-Air.local>
2017-02-21 15:26:23 +0800

    copying my previous code

--- db/schema.rb
+++ db/schema.rb
@@ -50,7 +50,6 @@ ActiveRecord::Schema.define(version: 20170221023238) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
-    t.string   "full_name"
     t.string   "email",                          null: false
     t.string   "encrypted_password", limit: 128
     t.string   "confirmation_token", limit: 128
