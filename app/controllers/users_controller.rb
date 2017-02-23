4329ca5331c523d5c6f92514554e686c178f32f6
coderamen <spencerteh@gmail.com>
2017-02-23 15:52:03 +0800

    Exe 150 1. Profile page able to create listing 2. Error message page, click profile page to return to check in my listing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,11 @@ class UsersController < Clearance::UsersController
   def show
     @user = User.find(params[:id])
     @listings = @user.listings
+    @new_listing = Listing.new
   end
 
   def index
-       @user = User.order(:id).page params[:page]
+    @user = User.order(:id).page params[:page]
   end
 
 end
